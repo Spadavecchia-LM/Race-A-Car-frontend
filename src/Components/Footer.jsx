f51b5c3c83b858a847d6f0cb34c7761da5f030b6
jmsantias <jmsantias99@gmail.com>
2024-02-18 21:25:16 -0300

    Footer v2

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import Logo from './utils/Logo.png';
 const Footer = () => {
   return (
     <footer className="bg-primaryBlue w-full fixed bottom-0 text-primaryWhite">
-      <div className="container mx-auto px-4 flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <img src={Logo} alt="Logo"/>
-          <div>&copy; {new Date().getFullYear()} Race-A-Car</div>
+          <div>&copy;{new Date().getFullYear()} Race-A-Car. Todos los derechos reservados.</div>
         </div>
       </div>
     </footer>
