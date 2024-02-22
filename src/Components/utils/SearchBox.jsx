ddaec07031c4081a74bd88adc14b46b392f3e1b8
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2024-02-21 23:50:36 -0300

    box shadow azul en inputs borrado

--- src/Components/utils/SearchBox.jsx
+++ src/Components/utils/SearchBox.jsx
@@ -6,7 +6,7 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const SearchBox = () => {
   return (
-    <div className='h-[auto] min-h-[150px] wrap mb-5 w-[90%] mx-auto bg-formGrey flex justify-center items-center rounded-2xl p-[16px]'>
+    <div className='h-[auto] min-h-[150px] wrap mb-5 mt-5 w-[90%] mx-auto bg-formGrey flex justify-center items-center rounded-2xl p-[16px]'>
  <form className='flex gap-10 items-center justify-center flex-wrap '>
       <div className='flex flex-col gap-2'>
         <label htmlFor="lugarRecogida">Lugar de recogida</label>
